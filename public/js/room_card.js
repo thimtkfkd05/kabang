eb57d163047bf1aaf3a99707cc0738d94afdacb3
thimtkfkd05 <thimtkfkd05@gmail.com>
2018-05-31 22:06:15 +0900

    divided room_list js

---
+++ public/js/room_card.js
@@ -0,0 +1,20 @@
+var roomcard = function(ref, loc, img, pri, typ) {
+  return [
+    '<div class="col-6">',
+      '<a href="', ref, '" class="btn btn-block btn-outline-light text-dark" role="button">',
+        '<div class="card">',
+          '<div class="card-header">',loc,'</div>',
+          '<div class="card-body">',
+            '<img class="card-img-top" src="',img,'" alt="Card image">',
+          '</div>',
+          '<div class="card-footer">',
+            '<div class="row">',
+              '<div class="col-6">',pri,'</div>',
+              '<div class="col-6">',typ,'</div>',
+            '</div>',
+          '</div>',
+        '</div>',
+      '</a>',
+    '</div>'
+  ].join('');
+};
