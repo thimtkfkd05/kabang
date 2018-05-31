1d911b8ce2deaa5a64e6319a6dfc293baf0a7908
thimtkfkd05 <thimtkfkd05@gmail.com>
2018-05-31 22:05:46 +0900

    fixed room list

--- public/js/script.js
+++ public/js/script.js
@@ -1,49 +1,8 @@
 $(document).ready(function() {
-  console.log("Hello World!");
-
-
-var roomcard = function(ref, loc, img, pri, typ) {
-  return [
-    '<div class="col-6">',
-      '<a href="', ref, '" class="btn btn-block btn-outline-light text-dark" role="button">',
-        '<div class="card">',
-          '<div class="card-header">',loc,'</div>',
-          '<div class="card-body">',
-            '<img class="card-img-top" src="',img,'" alt="Card image">',
-          '</div>',
-          '<div class="card-footer">',
-            '<div class="row">',
-              '<div class="col-6">',pri,'</div>',
-              '<div class="col-6">',typ,'</div>',
-            '</div>',
-          '</div>',
-        '</div>',
-      '</a>',
-    '</div>'
-  ].join('');
-};
-
-var roomNo = 7
-var room_list = []
-$.get('/roomlist', {}, function(res) {
-  if (res) {
-    room_list = res.result;
-  }
-})
-
-if(room_list.length == 0){ $('div.row_adder').append('<p>Result Not Found</p>') }
-else if(room_list.length == 1){ $('div.row_adder').append(roomcard('#','Location','img.jpg','Price','Type')) }
-else{
-  for (var i = 0; i < roomNo/2 ; i++) {
-    var html = '<div class="row">';
-    for (var j = 0; j < 2; j++){
-      html += roomcard('#', 'Location', 'img.jpg', 'Price', 'Type');
-      if(roomNo%2==1 && i > roomNo/2 -1){ break; }
-    }
-    html += '</div>';
-    $('div.row_adder').append(html);
-  }
-}
-
+  $('.btn-logout').click(function() {
+    $.post('/auth/logout', {}, function(result) {
+      href.location = '/login';
+    });
+  });
 });
 //<div class="div_class_wrapper"></div>
