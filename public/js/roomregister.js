67e0c15e04778ea9d510b97318045cb02c6fc427
thimtkfkd05 <thimtkfkd05@gmail.com>
2018-06-08 02:07:39 +0900

    fixed room register image upload bug

--- public/js/roomregister.js
+++ public/js/roomregister.js
@@ -51,60 +51,52 @@ function handleError (hasGeo, infoWindow, pos) {
   infoWindow.setContent(hasGeo ? 'Error: Geo service failed.':'Error: browser not support geo');
 }
 
-  function toggleBounce() {
-    if (marker.getAnimation() !== null) {
-      marker.setAnimation(null);
-    } else {
-      marker.setAnimation(google.maps.Animation.BOUNCE);
-    }
+function toggleBounce() {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
   }
-
-
+}
 
 // 이미지 정보들을 담을 배열
-var sel_files = [];
-var index = 0;
-var fileNameArr = new Array();
-
+var fileNameArr = [];
 
 $(document).ready(function() {
-    $("#input_imgs").on("change", handleImgFileSelect);
-    $("#input_imgs").on("change", function(){
-      fileNameArr.push($('#input_imgs').val());
-    });
+  $("#input_imgs").on("change", handleImgFileSelect);
 }); 
 
 function fileUploadAction() {
-    $("#input_imgs").trigger('click');
+    $("#input_imgs").click();
 }
 
 function handleImgFileSelect(e) {
-
     // 이미지 정보들을 초기화
 
-    var files = e.target.files;
-    var filesArr = Array.prototype.slice.call(files);
-    
-    
-    filesArr.forEach(function(f) {
-        if(!f.type.match("image.*")) {
-            alert("확장자는 이미지 확장자만 가능합니다.");
-            return;
-        }
+  var files = e.target.files;
+  var filesArr = Array.prototype.slice.call(files);
+  
+  filesArr.forEach(function(f) {
+    if(!f.type.match("image.*")) {
+      alert("확장자는 이미지 확장자만 가능합니다.");
+      return;
+    }
 
-        sel_files.push(f);
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      var index = fileNameArr.length;
+      var html = "<a onclick=\"deleteImageAction("+index+")\" id=\"img_id_"+index+"\"><img src=\"" + e.target.result + "\" data-file='"+f.name+"' class='selProductFile' ></a>";
+      $(".imgs_wrap").append(html);
+      fileNameArr.push(e.target.result);
+    };
+    reader.readAsDataURL(f);
+  });
+};
 
-        var reader = new FileReader();
-        reader.onload = function(e) {
-            var html = "<a href=\"javascript:void(0);\" onclick=\"deleteImageAction("+index+")\" id=\"img_id_"+index+"\"><img src=\"" + e.target.result + "\" data-file='"+f.name+"' class='selProductFile' ></a>";
-            $(".imgs_wrap").append(html);
-            $(".imgs_wrap").append(index);
-            index++;
-        }
-        reader.readAsDataURL(f);
-        
-    });
-}
+function deleteImageAction(index) {
+  fileNameArr.splice(index, 1);
+  $('#img_id_'+index).remove();
+};
 
 var optionNum;
 function addCheckbox(name) {
@@ -116,7 +108,7 @@ function addCheckbox(name) {
  $('<label />', { 'for': 'cb'+ optionNum, text:  name}).appendTo(container);
 }
 
-function getRoomInfo() {
+function getRoomInfo(next) {
   var chkedArr = new Array;
   $("input:checkbox:checked").each(function(index){
     chkedArr.push($(this).val());
@@ -128,10 +120,11 @@ function getRoomInfo() {
   }
 
   var geocoder = new google.maps.Geocoder;
+
   geocoder.geocode ({'location': location_obj}, function (results, status){
     if (status == 'OK') {
       if (results[1]) {
-        return {
+        return next({
           address : results[1].formatted_address,
           deposit : $('#deposit').val(),
           monthly : $('#monthly').val(),
@@ -142,14 +135,14 @@ function getRoomInfo() {
           picture : fileNameArr,
           location : location_obj,
           enrolled_date : new Date().toISOString()
-        };
+        });
       } else {
         console.log ('No results found');
-        return null;
+        return next(null);
       }
     } else {
       console.log ("Geocoder failed due to: " + status);
-      return null;
+      return next(null);
     }
   });
 };
@@ -161,41 +154,42 @@ $(document).ready(function() {
     }
   });
   $('#register').click(function(){
-    var room_info = getRoomInfo();
-    if (room_info) {
-      $.post('/register_room', room_info, function(result){
-        if(result){
-          alert("Register Success!!");
-          location.href = '/mypage';
-        } else {
-          alet("Register Fail!!");
-          location.reload();
-        }
-      });
-    } else {
-      alert("Register Fail!!");
-      location.reload();
-    }
+    getRoomInfo(function(room_info) {
+      if (room_info) {
+        $.post('/register_room', room_info, function(result){
+          if(result){
+            alert("Register Success!!");
+            location.href = '/mypage';
+          } else {
+            alet("Register Fail!!");
+            location.reload();
+          }
+        });
+      } else {
+        alert("Register Fail!!");
+        location.reload();
+      }
+    });
   });
 
   $('#edit').click(function(){
     var room_id = $(this).data('room-id');
-    var room_info = getRoomInfo();
-    
-    if (room_info && room_id) {
-      $.post('/register_room/'+room_id, room_info, function(result){
-        if(result){
-          alert("Edit Success!!");
-          location.href = '/mypage';
-        } else {
-          alet("Edit Fail!!");
-          location.reload();
-        }
-      });
-    } else {
-      alert("Edit Fail!!");
-      location.reload();
-    }
+    getRoomInfo(function(room_info) {
+      if (room_info && room_id) {
+        $.post('/register_room/'+room_id, room_info, function(result){
+          if(result){
+            alert("Edit Success!!");
+            location.href = '/mypage';
+          } else {
+            alet("Edit Fail!!");
+            location.reload();
+          }
+        });
+      } else {
+        alert("Edit Fail!!");
+        location.reload();
+      }
+    });
   });
 
   $('#delete').click(function() {
